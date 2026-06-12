3b707be90da1a9111ddf944621bcbabea7088501
TapanYadav000 <tohispace000@gmail.com>
2026-06-12 08:10:17 +0530

    feat(data): implement UserRepository

---
+++ app/src/main/java/com/tapan/aetherai/domain/repository/UserRepositoryImpl.kt
@@ -0,0 +1,22 @@
+package com.tapan.aetherai.data.repository
+
+import com.tapan.aetherai.data.datastore.UserPreferencesDataStore
+import com.tapan.aetherai.domain.model.UserProfile
+import com.tapan.aetherai.domain.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class UserRepositoryImpl @Inject constructor(
+    private val dataStore: UserPreferencesDataStore
+) : UserRepository {
+
+    override suspend fun saveUserProfile(
+        profile: UserProfile
+    ) {
+        dataStore.saveUserProfile(profile)
+    }
+
+    override fun getUserProfile(): Flow<UserProfile> {
+        return dataStore.getUserProfile()
+    }
+}
